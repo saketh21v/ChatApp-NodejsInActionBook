a3dc1db671b4b71fc1fcf06bdea4bd15937d3160
Saketh Vallakatla <saketh21v@gmail.com>
2017-02-23 18:57:59 +0530

    + Added .gitignore + Server partially implemented + Some additional static pages added

--- server.js
+++ server.js
@@ -10,8 +10,59 @@ var cache = {};
 
 // Functions for handling http requests
 // 404 Page doesn't exist error
-function send404(res){
-    res.writeHead(404, {'Content-Type': 'text/plain'});
-    res.write('Error 404: resource not found.');
-    respond.end();
+function send404(response){
+    response.writeHead(404, {'Content-Type': 'text/plain'});
+    response.write('Error 404: resource not found.');
+    response.end();
 }
+
+// File send function
+function sendFile(response, filePath, fileContents){
+    response.writeHead(
+        200,
+        {'Content-Type': mime.lookup(path.basename(filePath))}
+    );
+    response.end(fileContents);
+}
+
+// Serve static files after storing in cache
+function serveStatic(response, cache, absPath){
+    if(cache[absPath]){
+        sendFile(response, absPath, cache[absPath]);
+    } else{
+        fs.exists(absPath, function(exists){
+            if(exists){
+                fs.readFile(absPath, function(err, data){
+                    if(err){
+                        send404(response);
+                    } else{
+                        cache[absPath] = data;
+                        sendFile(response, absPath, cache[absPath]);
+                    }
+                });
+            } else{
+                send404(response);
+            }
+        });
+    }
+}
+
+// Server object 
+var server = http.createServer(function(request, response){
+    console.log("Received a request. Serving...");
+    var filePath = false;
+    if(request.url == '/'){
+        filePath = 'public/index.html';
+    } else{
+        filePath = 'public/'+request.url;
+    }
+    var absPath = './'+filePath;
+    serveStatic(response, cache, absPath);
+    console.log("Done.");
+    console.log("Server listening on port 3000...");
+});
+
+// Starting the server
+server.listen(3000, function(){
+    console.log("Server listening on port 3000.");
+});
